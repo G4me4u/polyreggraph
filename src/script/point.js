5f36cf08b48d5532ac49f63c1f5161b0b555a628
G4me4u <G4me4u@gmail.com>
2017-11-16 00:16:36 +0100

    Added a simple point class

---
+++ src/script/point.js
@@ -0,0 +1,29 @@
+class Point {
+
+	constructor(x = 0.0, y = 0.0) {
+		this.x = x;
+		this.y = y;
+	}
+
+	set(x, y) {
+		this.x = x;
+		this.y = y;
+		return this;
+	}
+
+	translate(x, y) {
+		this.x += x;
+		this.y += y;
+	}
+
+	distSqr(other) {
+		return (other.x - this.x) *
+			   (other.x - this.x) +
+			   (other.y - this.y) *
+		       (other.y - this.y);
+	}
+
+	dist(other) {
+		return Math.sqrt(distSqr(other));
+	}
+}
